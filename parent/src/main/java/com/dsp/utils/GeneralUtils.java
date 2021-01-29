1a48786cad9543c276e8479e00a1b69e2ea789c6
ycohen <yarinyc@gmail.com>
2021-01-29 19:25:43 +0200

    debug fix

--- parent/src/main/java/com/dsp/utils/GeneralUtils.java
+++ parent/src/main/java/com/dsp/utils/GeneralUtils.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class GeneralUtils {
 
-    public static Boolean DEBUG = false;
+    public static boolean DEBUG = true;
 
     public enum Counters{
         COUNTL,
