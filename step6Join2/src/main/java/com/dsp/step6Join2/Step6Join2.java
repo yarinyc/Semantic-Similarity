772f554dd087ce6641b5d9d6ee141da9cf8180b8
omerd <omerdv@gmail.com>
2021-01-31 15:46:48 +0200

    fixed step6Join2

--- step6Join2/src/main/java/com/dsp/step6Join2/Step6Join2.java
+++ step6Join2/src/main/java/com/dsp/step6Join2/Step6Join2.java
@@ -94,13 +94,14 @@ public class Step6Join2 {
                 //if the value is of tag "LF", i.e it is of <count(F=f,L=l),count(L=l)>
                 else{
                     String newKey = splitValue[1]; // <lexeme,feature>
-                    String[] counters = splitValue[2].split(",");
+                    String countLF = splitValue[2];
+                    String countL = splitValue[3];
                     //newValue=<count(F=f,L=l),count(L=l),count(F=f)>
-                    String newValue = counters[0]+","+countF;
+                    String newValue = countLF+"\t"+countL+"\t"+countF;
                     context.write(new Text(newKey), new Text(newValue));
                     //calculate all assoc values according to the formulas in the article
 //                    List<Long> counts = Arrays.stream(newValue.split(",")).map(Long::parseLong).collect(Collectors.toList());
-//                    AssocCalculator assocCalculator = new AssocCalculator(countAllLexemes, countAllFeatures, counts.get(1), counts.get(2), counts.get(0));
+//                    AssocCalculator assocCalculator = new AssocCalculator(countAllLexemes, countAllFeatures, Long.parseLong(countL), Long.parseLong(countF), Long.parseLong(countLF));
 //                    List<Number> assocs = assocCalculator.getAllAssocValues();
 //                    context.write(new Text(newKey), new Text(assocs.toString()));
                 }
@@ -111,13 +112,21 @@ public class Step6Join2 {
     public static class PartitionerClass extends Partitioner<Text, Text> {
         @Override
         public int getPartition(Text key, Text value, int numPartitions) {
-            // foreign key is the lexeme (split key at index 0)
-            String lexeme = key.toString().split(",")[0];
-            return Math.abs(lexeme.hashCode() % numPartitions);
+            // foreign key is the feature
+            String featureKey;
+            String[] splitKey = key.toString().split(",");
+            if(splitKey.length == 1){
+                featureKey = splitKey[0];
+            }
+            else{
+                featureKey = splitKey[1];
+            }
+            GeneralUtils.logPrint("In partitioner: key = " + key.toString() + " , featureKey is = " + featureKey);
+            return Math.abs(featureKey.hashCode() % numPartitions);
         }
     }
 
-    // A comparator for text, to make sure the K-V pair of the lexeme count (Count(F=f)) comes to the reducer before
+    // A comparator for text, to make sure the K-V pair of the feature count (Count(F=f)) comes to the reducer before
     // all <lexeme,feature> counts (Count(L=l,F=f))
     public static class TextKeyComparator extends WritableComparator {
         protected TextKeyComparator() {
@@ -130,25 +139,54 @@ public class Step6Join2 {
             Text key2 = (Text) o2;
             String[] split1 = key1.toString().split(",");
             String[] split2 = key2.toString().split(",");
-            int compareVal = split1[0].compareTo(split2[0]);
             //split key with length of 1 is of count(F=f), so it comes first in order
-            if(compareVal==0){
-                if(split1.length == 1 && split2.length == 2){
-                    GeneralUtils.logPrint("in TextComparator: received key = " + split1[0]);
+            GeneralUtils.logPrint("in TextComparator: received keys = " + key1.toString() + " , " + key2.toString());
+            if(split1.length == 1 && split2.length == 2){
+                GeneralUtils.logPrint("in TextComparator: case 1 - F & LF");
+                int compareVal = split1[0].compareTo(split2[1]);
+                if(compareVal == 0){
                     return -1;
                 }
-                else if(split1.length == 2 && split2.length == 1){
+                else{
+                    return compareVal;
+                }
+            }
+            else if(split1.length == 2 && split2.length == 1){
+                GeneralUtils.logPrint("in TextComparator: case 2 - LF & F");
+                int compareVal = split1[1].compareTo(split2[0]);
+                if(compareVal == 0){
                     return 1;
                 }
-                else return 0;
+                else {
+                    return compareVal;
+                }
             }
-            else{
-                return compareVal;
+            else if(split1.length == 2 && split2.length == 2) {
+                GeneralUtils.logPrint("in TextComparator: case 3 - LF & LF");
+                int compareVal = split1[1].compareTo(split2[1]);
+                if(compareVal == 0){
+                    return 0;
+                }
+                else {
+                    return compareVal;
+                }
             }
+            else if(split1.length == 1 && split2.length == 1) {
+                GeneralUtils.logPrint("in TextComparator: case 4 - F & F");
+                int compareVal = split1[0].compareTo(split2[0]);
+                if(compareVal == 0){
+                    return 0;
+                }
+                else {
+                    return compareVal;
+                }
+            }
+            GeneralUtils.logPrint("in TextComparator: error in received keys");
+            return 0; //this should not return;
         }
     }
 
-    // Grouping comparator to make sure all lexeme and <lexeme,feature> keys arrive to the same reducer according only to lexeme
+    // Grouping comparator to make sure all feature and <lexeme,feature> keys arrive to the same reducer according only to feature
     public static class GroupingComparator extends WritableComparator {
         protected GroupingComparator() {
             super(Text.class, true);
@@ -158,10 +196,23 @@ public class Step6Join2 {
         public int compare(WritableComparable o1, WritableComparable o2) {
             Text key1 = (Text) o1;
             Text key2 = (Text) o2;
-            String lexeme1 = key1.toString().split(",")[0];
-            String lexeme2 = key2.toString().split(",")[0];
-            GeneralUtils.logPrint("in GroupingComparator: received keys = " + key1.toString() + " " + key2.toString() + " compared: " + lexeme1.compareTo(lexeme2));
-            return lexeme1.compareTo(lexeme2);
+            String[] splitKey1 = key1.toString().split(",");
+            String[] splitKey2 = key2.toString().split(",");
+            GeneralUtils.logPrint("in GroupingComparator: received keys = " + key1.toString() + " , " + key2.toString());
+            if(splitKey1.length == 1 && splitKey2.length == 2){
+                return splitKey1[0].compareTo(splitKey2[1]);
+            }
+            else if(splitKey1.length == 2 && splitKey2.length == 1){
+                return splitKey1[1].compareTo(splitKey2[0]);
+            }
+            else if(splitKey1.length == 2 && splitKey2.length == 2) {
+                return splitKey1[1].compareTo(splitKey2[1]);
+            }
+            else if(splitKey1.length == 1 && splitKey2.length == 1) {
+                return splitKey1[0].compareTo(splitKey2[0]);
+            }
+            GeneralUtils.logPrint("in TextComparator: error in received keys");
+            return 0; //this should not return;
         }
     }
 
